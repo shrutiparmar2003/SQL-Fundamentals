57c3d1647409150405615548ee471c7148a8e502
shrutiparmar2003 <shrutiparmar01082003@gmail.com>
2025-04-22 15:26:16 +0530

    show create open databases

---
+++ show_create_open_database.sql
@@ -0,0 +1,4 @@
+show databases;   -- This shows all the databases in the system
+CREATE DATABASE tech_ecommerce;  -- This command creates a new database
+USE tech_ecommerce; -- This is for opening  an existing database
+
